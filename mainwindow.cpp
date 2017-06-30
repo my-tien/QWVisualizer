103cf842939de504c8a7848fd874ce9619ef8701
My-Tien Nguyen <adtieni@gmail.com>
2017-06-30 17:54:16 +0200

    More readable representation of the data
    Vertical sliders and minimum sized controls to give graphs more horizontal space to expand.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,15 @@
 #include "classicalwalker.h"
 #include "quantumwalker.h"
 
+#include <QAbstract3DGraph>
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     for (auto & graph : {&cGraph, &qGraph}) {
         graph->setFlags(graph->flags() ^ Qt::FramelessWindowHint);
         graph->columnAxis()->setRange(0,6);
         graph->valueAxis()->setRange(0, 1);
         auto graphWidget = QWidget::createWindowContainer(graph, this);
-        graphWidget->setMinimumSize(500, 500);
+        graph->setSelectionMode(QtDataVisualization::QAbstract3DGraph::SelectionFlags(QtDataVisualization::QAbstract3DGraph::SelectionSlice | QtDataVisualization::QAbstract3DGraph::SelectionRow));
         graphLayout.addWidget(graphWidget);
     }
     int row = 0;
@@ -31,20 +33,19 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
         spin.setRange(min, max);
         slider.setRange(spin.minimum(), spin.maximum());
         spin.setValue(value);
-        slider.setOrientation(Qt::Horizontal);
-        controlsLayout.addWidget(&spin, row, 0);
+        spin.setFixedWidth(spin.sizeHint().width());
+        slider.setOrientation(Qt::Vertical);
+        controlsLayout.addWidget(&spin, row, 0, Qt::AlignTop);
         controlsLayout.addWidget(&slider, row++, 1);
     };
     addNumberControl(pathLenSpin, pathLenSlider, 7, 1, 100, "length: ", "");
     addNumberControl(stepsSpin, stepsSlider, 3, 1, 1000, "", " steps");
     controlsLayout.setAlignment(Qt::AlignTop);
-    graphDummyWidget.setLayout(&graphLayout);
-    controlsDummyWidget.setLayout(&controlsLayout);
-    split.addWidget(&graphDummyWidget);
-    split.addWidget(&controlsDummyWidget);
-    mainLayout.addWidget(&split);
+    mainLayout.addLayout(&graphLayout);
+    mainLayout.addLayout(&controlsLayout);
     setCentralWidget(new QWidget(this));
     centralWidget()->setLayout(&mainLayout);
+    setMinimumSize(500, 500);
     simulate();
 }
 
@@ -69,6 +70,7 @@ void MainWindow::simulate() {
         graph.valueAxis()->setRange(0, max);
         series->dataProxy()->addRow(data);
         graph.addSeries(series);
+        graph.seriesList().at(0)->setSelectedBar({0, 0});
     };
     sim(cWalker, cGraph);
     sim(qWalker, qGraph);
